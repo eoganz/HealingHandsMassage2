b70bddc7ba634485b6fd078b243e95af2c35b63d
eoganz <jakeemettler@gmail.com>
2019-08-14 14:19:55 -0700

    Created Roles

--- HealingHandsMassage/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ HealingHandsMassage/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -53,7 +53,7 @@ namespace HealingHandsMassage.Areas.Identity.Pages.Account
             public string Phone { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
