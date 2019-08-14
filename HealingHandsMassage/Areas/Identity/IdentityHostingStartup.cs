d336d7d7d89d62072eac07ab9634cbe63d64ffc4
eoganz <jakeemettler@gmail.com>
2019-08-14 13:23:15 -0700

    Generated scaffolding and fixed some css

---
+++ HealingHandsMassage/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HealingHandsMassage.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HealingHandsMassage.Areas.Identity.IdentityHostingStartup))]
+namespace HealingHandsMassage.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
